05c0182cae03a73b98b8c268e614053b4bf2ee47
Ezgi Zeren <ezgi.zeren@outlook.com>
2022-07-11 16:53:12 -0700

    Updating reflection.py for sharding.

--- sqlalchemy_singlestore/reflection.py
+++ sqlalchemy_singlestore/reflection.py
@@ -5,13 +5,67 @@ from __future__ import annotations
 import re
 
 from sqlalchemy import log
+from sqlalchemy import util
 from sqlalchemy.dialects.mysql.reflection import _re_compile
 from sqlalchemy.dialects.mysql.reflection import MySQLTableDefinitionParser
 
+class ReflectedState(object):
+    """Stores raw information about a SHOW CREATE TABLE statement."""
+
+    def __init__(self):
+        self.columns = []
+        self.table_options = {}
+        self.table_name = None
+        self.keys = []
+        self.fk_constraints = []
+        self.ck_constraints = []
 
 @log.class_logger
 class SingleStoreTableDefinitionParser(MySQLTableDefinitionParser):
     """Parses the results of a SHOW CREATE TABLE statement."""
+
+    def _parse_constraints(self, line):
+        type_, spec = super(SingleStoreTableDefinitionParser, self)._parse_constraints(line)
+        re_shard = _re_compile(r'\s+,\s+SHARD\s+KEY\s+\(\)\s+')
+        m = re_shard.match(line)
+        if m:
+            type_ = "shard_key"
+            spec = {'type': None, 'name': 'SHARD', 'using_pre': None, 'columns': [], 'using_post': None, 'keyblock': None, 'parser': None, 'comment': None, 'version_sql': None}
+        return type_, spec
+            
+    def parse(self, show_create, charset):
+        state = ReflectedState()
+        state.charset = charset
+        for line in re.split(r"\r?\n", show_create):
+            if line.startswith("  " + self.preparer.initial_quote):
+                self._parse_column(line, state)
+            # a regular table options line
+            elif line.startswith(") "):
+                self._parse_table_options(line, state)
+            # an ANSI-mode table options line
+            elif line == ")":
+                pass
+            elif line.startswith("CREATE "):
+                self._parse_table_name(line, state)
+            # Not present in real reflection, but may be if
+            # loading from a file.
+            elif not line:
+                pass
+            else:
+                type_, spec = self._parse_constraints(line)
+                if type_ is None:
+                    util.warn("Unknown schema content: %r" % line)
+                elif type_ == "key":
+                    state.keys.append(spec)
+                elif type_ == "fk_constraint":
+                    state.fk_constraints.append(spec)
+                elif type_ == "ck_constraint":
+                    state.ck_constraints.append(spec)
+                elif type_ == "shard_key":
+                    state.keys.append(spec)
+                else:
+                    pass
+        return state
 
     def _prep_regexes(self) -> None:
         """Pre-compile regular expressions."""
